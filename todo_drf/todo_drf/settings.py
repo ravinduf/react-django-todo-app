faa378c99dd22b940a91188ca3d624eff01e6ee4
raavinduf <ravinduf@rocketmail.com>
2020-06-23 15:21:05 +0530

    added cors-headers and allowed react to access django rest api

--- todo_drf/todo_drf/settings.py
+++ todo_drf/todo_drf/settings.py
@@ -41,10 +41,12 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
     
     'rest_framework',
+    'corsheaders',
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'todo_drf.urls'
@@ -123,3 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_WHITELIST = [
+#     "http://localhost:3000",
+# ]
+
+
+
+CORS_ORIGIN_WHITELIST=[ 'https://localhost:3000' ]
